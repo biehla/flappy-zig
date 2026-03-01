5c40b2f90ba18aad52a09e9a0d38befe9cd68c82
Anand Biehl <36610944+biehla@users.noreply.github.com>
2026-02-28 22:11:20 -0500

    Scene loader all done. Idk what problem this was supposed to solve anymore

--- src/scenes.zig
+++ src/scenes.zig
@@ -21,60 +21,39 @@ pub fn displayScene(scene: Scenes, camera: rl.Camera2D) void {
 }
 
 fn getScenes() anyerror!void {
+    // Set up handles for the scene directory and zon output file
     var scenes_dir_handle = try fs.cwd().openDir("./scenes/", .{ .iterate = true });
-    defer scenes_dir_handle.close();
     var scenes_dir_contents = scenes_dir_handle.iterate();
-    var scenes_zon_handle = scenes_dir_handle.createFile("../scenes.zon", .{}) catch |err| {
-        std.log.err("poop", .{});
-        std.log.err("poop {any}", .{err});
-        std.debug.print("poop {any}", .{err});
-        return fs.Dir.OpenError.FileNotFound;
-    };
-    // var buffer: [100]u8 = undefined;
-    // var fileWriter = scenes_zon_handle.writer(&buffer);
+    var scenes_zon_handle = try scenes_dir_handle.createFile("../scenes.zon", .{});
+    defer scenes_dir_handle.close();
     defer scenes_zon_handle.close();
 
+    // Set up memory allocators to temporarily store scene list
     var arena: std.heap.ArenaAllocator = .init(std.heap.page_allocator);
-    defer arena.deinit();
     const allocator = arena.allocator();
+    var scene_files: std.ArrayList([]const u8) = try std.ArrayList([]const u8).initCapacity(allocator, 2);
+    defer arena.deinit();
+    defer scene_files.deinit(allocator);
 
-
-
+    // Setup string writer 
     var writer: std.io.Writer.Allocating = try .initCapacity(allocator, 5);
     defer writer.deinit();
 
-    var scene_files: std.ArrayList([]const u8) = try std.ArrayList([]const u8).initCapacity(allocator, 3);//try std.ArrayList([]const u8).initCapacity(allocator, 1);
-    defer scene_files.deinit(allocator);
-
     while (try scenes_dir_contents.next()) |scene| {
-        const name: []const u8 = try std.mem.Allocator.dupe(allocator, u8, scene.name);
         switch (scene.kind) {
             .file => {
-                try scene_files.append(allocator, name);
-                std.log.err("Saving node: {s}", .{name});
-
-                // var new_scene: SceneFile = .{ .data = scene.name };
+                try scene_files.append(allocator, try std.mem.Allocator.dupe(allocator, u8, scene.name));
+                std.log.err("Saving node: {s}", .{scene.name});
             },
             else => std.log.err("NOT Saving NOde: {s}", .{scene.name}),
         }
     }
 
-
-
-    // var curr = scene_files.first.?;
-
-    // scene_files.
-
-
-    while (scene_files.pop()) |file| {
-        // const node_data: *SceneFile = @fieldParentPtr("node", curr);
-        std.log.err("Logging node: {s}", .{file});
-
-        try std.zon.stringify.serialize(file, .{}, &writer.writer);
-    }
+    try std.zon.stringify.serialize(scene_files, .{}, &writer.writer);
 
 
     const written = writer.written();
+    _ = try scenes_zon_handle.write(written);
     std.log.err("Log: {s}" , .{written});
 }
 
